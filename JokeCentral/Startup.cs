4095f8e86eaa46464760523c1ca629a07e8e3a4f
Mateusz Rumian <mrumian@sumologic.com>
2021-09-21 13:13:58 +0200

    Bump OT, add env vars

--- JokeCentral/Startup.cs
+++ JokeCentral/Startup.cs
@@ -1,7 +1,10 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 using OpenTelemetryExtensions.HttpClient;
 
@@ -12,13 +15,26 @@ namespace JokeCentral
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {
+        {   
+            
+            // If your application is .NET Standard 2.1 or above, and you are using an insecure (http) endpoint,
+            // the following switch must be set before adding Exporter.
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            
+            var resource = ResourceBuilder.CreateDefault().AddEnvironmentVariableDetector();
+            
             services.AddControllersWithViews();
             services.AddOpenTelemetryTracing(builder =>
                 builder
+                    .SetResourceBuilder(resource)
                     .AddAspNetCoreInstrumentation()
                     .AddHttpClientInstrumentation(options =>
                         options.Enrich = ActivityNameEnricher.Enrich)
+                    .AddOtlpExporter(otlpOptions =>
+                    {
+                        otlpOptions.Endpoint = new Uri(System.Environment.GetEnvironmentVariable(
+                            "OTEL_EXPORTER_OTLP_ENDPOINT") ?? "http://localhost:4317");
+                    })
                     .AddConsoleExporter()
             );
         }
